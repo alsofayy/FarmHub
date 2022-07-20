5ddb9927c498e9a0553964cd850cd519ed2ad089
Fayy <101954998+alsofayy@users.noreply.github.com>
2022-07-21 09:07:32 +1200

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,13 +1,3 @@
-if getgenv().FarmHubLoaded then
-    game:GetService("StarterGui"):SetCore("SendNotification", {
-        Title = "FarmHub",
-        Text = "Already Loaded!",
-        Duration = 5,
-        Button1 = "Ok",
-    })
-	return
-end
-
 if game.PlaceId == 606849621 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/alsofayy/FarmHub/master/Games/Jailbreak.lua"))()
 elseif game.PlaceId == 606849621 then
@@ -17,5 +7,3 @@ elseif game.PlaceId == 2534724415 then
 elseif game.PlaceId == 537413528 then
     loadstring(game:HttpGet("https://raw.githubusercontent.com/alsofayy/FarmHub/master/Games/Babft.lua"))()
 end
-
-getgenv().FarmHubLoaded = true
